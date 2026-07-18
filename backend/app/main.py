0dd6fb90aaaf9bea160ee0a371168aa2b07c91d0
JaypeeDLVG <john.philip.dela.vega@ph.ey.com>
2026-07-18 16:47:42 +0800

    update on cors to get from .env

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,15 +3,19 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 from app.schemas import QueryRequest
 from app.rag.chain import rag_chain
-import logging
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
+origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
 
 
-logger = logging.getLogger("uvicorn.error")
 app = FastAPI(title="RAG API")
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # for development; restrict this in production
+    allow_origins=origins,  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -23,7 +27,6 @@ def health_check():
 
 @app.post("/ask/stream")
 def ask_stream(request: QueryRequest):
-    logger.info(f"Initiating stream for db: '{request.db_name}' | Question: '{request.question}'")
     
     chain_input = {
             "question": request.question,
